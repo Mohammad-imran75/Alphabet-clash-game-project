5b6cd70088a27cfa93688093ae0430840f1f76af
Mohammad-imran75 <imranbd7531@gmail.com>
2024-02-17 20:13:31 +0600

    added the all section

--- script-clash/index.js
+++ script-clash/index.js
@@ -5,6 +5,62 @@
 //     const playGroundScreen = document.getElementById('play-ground-screen');
 //     playGroundScreen.classList.remove('hidden')
 // }
+function handleKeyBoardPress(event){
+    const keyPressed = event.key;
+    // console.log(keyPressed)
+    if(keyPressed === 'escape'){
+        gameOver();
+    }
+
+    document.addEventListener('keyup',handleKeyBoardPress);
+const currentAlphabetElementId = document.getElementById('current-alphabet');
+const currentAlphabetElement = currentAlphabetElementId.innerText;
+const currentAlphabet = currentAlphabetElement.toLowerCase();
+// console.log(currentAlphabet);
+
+if(keyPressed === currentAlphabet){
+    console.log('you win');
+    removeBackgroundColor(currentAlphabet);
+    continueGame();
+     
+    const currentValue = getTextValueElementById('current-score')
+    const newValue = currentValue + 1;
+    setTextValueElementById('current-score',newValue);
+
+
+
+    // const currentScoreElementById = document.getElementById('current-score');
+    // const currentScoreElementText = currentScoreElementById.innerText;
+    // const currentScore =parseInt(currentScoreElementText);
+    // const newScore = currentScore + 1;
+    
+    // currentScoreElementById.innerText = newScore;
+}else{
+    const updatedLife =getTextValueElementById('current-life');
+    const newLife = updatedLife - 1;
+    setTextValueElementById('current-life',newLife); 
+
+    if(newLife === 0){
+        gameOver();
+    }
+
+
+
+    // console.log('you loss a life')
+    // const currentLifeElementById = document.getElementById('current-life')
+    // const currentLifeElementText = currentLifeElementById.innerText;
+    // const currentLife = parseInt(currentLifeElementText);
+    // const newLife = currentLife - 1;
+
+    // currentLifeElementById.innerText = newLife;
+}
+}
+
+document.addEventListener('keyup',handleKeyBoardPress);
+
+
+
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
     console.log(alphabet);
@@ -17,6 +73,19 @@ function continueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('final-score')
     showElementById('play-ground-screen');
+    setTextValueElementById('current-life',5);
+    setTextValueElementById('current-score',0);
     continueGame();
+}
+function gameOver(){
+    hideElementById('play-ground-screen');
+    showElementById('final-score');
+
+    const lastScore = getAlphabetById('current-score');
+    setTextValueElementById('game-score',lastScore);
+
+    const currentAlphabet = getAlphabetById('current-alphabet');
+    removeBackgroundColor(currentAlphabet);
 }
--- script-clash/utility.js
+++ script-clash/utility.js
@@ -11,14 +11,33 @@ function setBackGroundColorById(elementId){
     const color = document.getElementById(elementId);
     color.classList.add('bg-orange-400');
 }
+function removeBackgroundColor(elementId){
+    const color = document.getElementById(elementId);
+    color.classList.remove('bg-orange-400');
+}
+function getTextValueElementById(elementId){
+    const valueElementId = document.getElementById(elementId);
+    const valueText = valueElementId.innerText;
+    const value = parseInt(valueText);
+    return value;
+}
+function getAlphabetById(elementId){
+    const currentAlphabet =document.getElementById(elementId);
+    const text = currentAlphabet.innerText
+    return text ;
+}
+function setTextValueElementById(elementId,value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
 function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    console.log(alphabets);
+    // console.log(alphabets);
 
     const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
-    console.log(index);
+    // console.log(index);
     const alphabet = alphabets[index];
     return alphabet;
 }
